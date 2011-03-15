b07ff90cb8e6cba6d8d53df2181b3bbbd7cce33f
Henning Hraban Ramm <hraban@fiee.net>
2011-03-15 11:27:56 +0100

    much better setup.py, using setuptools

--- setup.py
+++ setup.py
@@ -1,42 +1,23 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-from distutils.core import setup
+from setuptools import setup, find_packages
 import os
 
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir:
-    os.chdir(root_dir)
-
-for dirpath, dirnames, filenames in os.walk('temporale'):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-    if '__init__.py' in filenames:
-        pkg = dirpath.replace(os.path.sep, '.')
-        if os.path.altsep:
-            pkg = pkg.replace(os.path.altsep, '.')
-        packages.append(pkg)
-    elif filenames:
-        prefix = dirpath[13:] # Strip "registration/" or "registration\"
-        for f in filenames:
-            data_files.append(os.path.join(prefix, f))
-
-
 setup(name='fiee-temporale',
-      version='0-0-0',
+      version='0.0.0a1',
       description=u'Generic events for your django models',
+      keywords='event date calendar generic attachment',
       author='Henning Hraban Ramm',
       author_email='hraban@fiee.net',
+      license='BSD',
       url='https://github.com/fiee/fiee-temporale',
       download_url='https://github.com/fiee/fiee-temporale/tarball/master',
-      package_dir={'temporale': 'temporale'},
-      packages=packages,
-      package_data={'temporale': data_files},
+      package_dir={'temporale': 'temporale',},
+      packages=find_packages(),
+      include_package_data = True,
+      package_data = {'': ['*.rst', 'locale/*/LC_MESSAGES/*.*', 'templates/*/*.*', 'templates/*/*/*.*', ]},
       # see http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=['Development Status :: 2 - Pre-Alpha',
+      classifiers=['Development Status :: 3 - Alpha',
                    'Environment :: Web Environment',
                    'Framework :: Django',
                    'Intended Audience :: Developers',
@@ -46,6 +27,7 @@ setup(name='fiee-temporale',
                    'Topic :: Software Development :: Libraries :: Python Modules',
                    'Topic :: Utilities',
                    'Natural Language :: English',
-                   'Natural Language :: German'],
-      install_requires=['Django>=1.2', 'django-registration', 'fiee-dorsale'],
+                   'Natural Language :: German',],
+      install_requires=['Django>=1.2', 'django-registration', 'fiee-dorsale',],
+      zip_safe=False,
       )
