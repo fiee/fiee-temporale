d8cf85fcc43156eda6d7fa8cb407438197a9a0c3
Henning Hraban Ramm <hraban@fiee.net>
2011-11-01 14:36:33 +0100

    setup template path

--- demo/settings.py
+++ demo/settings.py
@@ -118,11 +118,10 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'demo.urls'
 
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
+TEMPLATE_DIRS = [
+    os.path.join(PROJECT_ROOT, 'templates'),
+]
+
 
 INSTALLED_APPS = (
     'django.contrib.auth',
