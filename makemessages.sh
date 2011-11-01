4c17b9b7aee6a1647da606a6c1ed01f5c22f50ad
Henning Hraban Ramm <hraban@fiee.net>
2011-11-01 16:11:06 +0100

    script to create/update locales

---
+++ makemessages.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+for DIR in temporale demo demo/biography
+do
+  cd $DIR
+  echo $DIR
+  ./manage.py makemessages -a -e html,py,tex,txt
+  ./manage.py makemessages -a -d djangojs
+  open locale/*/LC_MESSAGES/*.po
+  cd ..
+done
+cd ..
