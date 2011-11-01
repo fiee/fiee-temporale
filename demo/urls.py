8bbedcbc36339714929b64818ca29888f722fad4
Henning Hraban Ramm <hraban@fiee.net>
2011-11-01 16:03:33 +0100

    setup i18n and static urls

--- demo/urls.py
+++ demo/urls.py
@@ -1,18 +1,36 @@
+from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
+js_info_dict = {
+    'packages': ('temporale','demo','demo.biography',),
+}
+
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'demo.views.IndexView', name='demo-index'),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='demo-index'),
     url(r'^temporale/', include('temporale.urls')),
     url(r'^bio/', include('demo.biography.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^i18n/', include('django.conf.urls.i18n')),
+    url(r'^jsi18n/$', 'django.views.i18n.javascript_catalog', js_info_dict),
 )
+
+# serve static content in debug mode
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+            'show_indexes' : True
+        }),
+        (r'^(?P<path>favicon.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+        (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    )
