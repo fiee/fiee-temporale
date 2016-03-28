b38b479d607a30899e1cccf15358e8c038de870b
Henning Hraban Ramm <hraban@fiee.net>
2016-03-28 16:11:08 +0600

    fix path hierachy

--- makemessages.sh
+++ makemessages.sh
@@ -3,8 +3,8 @@ for DIR in temporale demo demo/biography
 do
   cd $DIR
   echo $DIR
-  ./manage.py makemessages -a -e html,py,tex,txt
-  ./manage.py makemessages -a -d djangojs
+  ../manage.py makemessages -a -e html,py,tex,txt
+  ../manage.py makemessages -a -d djangojs
   open locale/*/LC_MESSAGES/*.po
   cd ..
 done
