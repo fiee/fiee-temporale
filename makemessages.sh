5f70dcee576206a7d7c4cbe07492def51ffa8860
Henning Hraban Ramm <hraban@fiee.net>
2016-03-28 17:07:03 +0600

    locale update

--- makemessages.sh
+++ makemessages.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-for DIR in temporale demo demo/biography
+for DIR in temporale demo
 do
   cd $DIR
   echo $DIR
   ../manage.py makemessages -a -e html,py,tex,txt
-  ../manage.py makemessages -a -d djangojs
+  ../manage.py makemessages -a -e js -d djangojs
   open locale/*/LC_MESSAGES/*.po
   cd ..
 done
