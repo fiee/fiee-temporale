b2087f2b7a424e9cfdf025ce3bd09e5db4f1ea4d
Henning Hraban Ramm <hraban@fiee.net>
2016-04-15 17:29:12 +0600

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 import os
 
 setup(name='fiee-temporale',
-      version='0.0.5',
+      version='0.0.7',
       description=u'Generic events for your django models',
       keywords='event date calendar generic attachment',
       author='Henning Hraban Ramm',
@@ -28,6 +28,6 @@ setup(name='fiee-temporale',
                    'Topic :: Utilities',
                    'Natural Language :: English',
                    'Natural Language :: German',],
-      install_requires=['Django>=1.8', 'vobject', 'django-registration>=2', 'fiee-dorsale>=0.0.9',],
+      install_requires=['Django>=1.8', 'vobject', 'django-registration>=2', 'fiee-dorsale>=0.1.0',],
       zip_safe=False,
       )
---
+++ temporale/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.utils.translation import ugettext_lazy as _
+
+verbose_name = _(u'Temporale')
+verbose_name_plural = _(u'Temporales')
